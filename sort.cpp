140d5fda626f0ae53f7c1b3940cf76f27e6de62a
AMALO25 <subhanfuad@gmail.com>
2016-11-17 15:08:07 +0700

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -8,7 +8,7 @@ int main()
     int arr[]={2,8,4,1,9},sizeofarr=sizeof(arr)/sizeof(arr[0])-1,c; //Declaration
     int *early = arr;
     int *ending=arr+sizeof(arr)/sizeof(arr[0]);
-    for(int i=sizeofarr;i>=0;i--){
+    for(int i=sizeofarr;i>0;i--){
         c=*min_element(arr,arr+i); // Value of C is min number from array of arr
         cout<<c; //Print number to display
         remove (early, ending, c); //Remove min number after print on display
